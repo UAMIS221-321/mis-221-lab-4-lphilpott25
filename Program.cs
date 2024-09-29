ff0a83f5f880ee83826bf2d26d448a8a30852963
Liam Philpott <lphilpott@crimson.ua.edu>
2024-09-29 15:29:34 -0500

    finished

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
 ﻿string userInput = "";
-
-
+ 
+ 
 while (userInput != "4")
 {
     DisplayMenu();
     userInput = GetUserChoice();
     RoutEm(userInput);
 }
-
-
-
+ 
+ 
+ 
 static void DisplayMenu()
 {
     Console.Clear();
     System.Console.WriteLine(" 1. Display Plain Pizza \n 2. Displa Cheese Pizza\n 3. Display Pepperioni Pizza \n 4. Exit ");
 }
-
+ 
 static string GetUserChoice()
 {
     System.Console.WriteLine("Enter your menu choice:");
     return Console.ReadLine();
 }
-
+ 
 static void RoutEm(string userChoice)
 {
     if (userChoice == "1")
@@ -36,22 +36,86 @@ static void RoutEm(string userChoice)
     {
         DisplayPepperoniPizza();
     }
-    else{
+    else
+    {
         DisplayMenu();
     }
 }
-
+ 
 static void DisplayPlainPizza()
 {
     Random random = new Random();
-    int randomRows = random.Next(8,13);
-
-    System.Console.WriteLine();
-    for(int i = randomRows; i>=0; i--)
+    int randomRows = random.Next(8, 13);
+ 
+    System.Console.WriteLine(randomRows);
+    for (int i = randomRows; i > 0; i--)
     {
-        for(int j = 0; j<i+1; j++)
+        for (int j = 0; j < i; j++)
         {
             System.Console.Write("*");
+        }
+        System.Console.WriteLine();
+    }
+    Pause();
+}
+ 
+ 
+ 
+static void DisplayCheesePizza()
+{
+    Random random = new Random();
+    int randomRows = random.Next(8, 13);
+    for (int i = randomRows; i > 0; i--)
+    {
+        for (int j = 0; j < randomRows; j++)
+        {
+            if (j == 0 || j == i - 1 || i == randomRows)
+            {
+                System.Console.Write("*   ");
+            }
+            else if (j < i)
+            {
+                System.Console.Write("#   ");
+            }
+            else
+            {
+                System.Console.Write("    ");
+            }
+        }
+        System.Console.WriteLine();
+    }
+    Pause();
+}
+ 
+ 
+static void DisplayPepperoniPizza()
+{
+    Random random = new Random();
+    int randomRows = random.Next(8, 13);
+    System.Console.WriteLine();
+    for (int i = randomRows; i > 0; i--)
+    {
+        for (int j = 0; j < randomRows; j++)
+        {
+            if (j == 0 || j == i - 1 || i == randomRows)
+            {
+                System.Console.Write("*    ");
+            }
+            else if (j < i)
+            {
+                if (random.Next(0, 2) == 0) // Randomly decide to place a pepperoni or not
+                {
+                    System.Console.Write("[]   ");
+                }
+                else
+                {
+                    System.Console.Write("#    ");
+                }
+            }
+            else
+            {
+                System.Console.Write("     ");
+            }
         }
         System.Console.WriteLine();
     }
@@ -61,14 +125,4 @@ static void Pause()
 {
     System.Console.WriteLine("press any key to continue");
     Console.ReadKey();
-}
-
-
-static void DisplayCheesePizza()
-{
-
-}
-static void DisplayPepperoniPizza()
-{
-
 }
--- obj/Debug/net8.0/mis-221-lab-4-lphilpott25.AssemblyInfo.cs
+++ obj/Debug/net8.0/mis-221-lab-4-lphilpott25.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mis-221-lab-4-lphilpott25")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d49459b1cd54ebb5b00644f431ef56c3b4d3887b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b4d0eeb7959456e57e1886162b10121ff6f6038")]
 [assembly: System.Reflection.AssemblyProductAttribute("mis-221-lab-4-lphilpott25")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mis-221-lab-4-lphilpott25")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
